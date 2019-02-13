234ce9f9ac53bc64a8654cb6d4d2584972b142eb
mr.ShahAbbas <abdidevar@gmail.com>
2019-02-13 13:32:51 +0330

    new

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import React, {Component} from 'react';
+import ReactDOM from 'react-dom'
+
+
+class Login extends React.Component{
+    render(){
+        return (
+            <div>
+                <p>hello</p>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <Login />,
+    document.getElementById('root')
+  );
+  
